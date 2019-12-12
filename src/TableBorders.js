c9637ebc11e00cf87ff1155fa7d57db745e72780
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2019-12-12 22:11:36 +0100

    Make setting table borders more simple

--- src/TableBorders.js
+++ src/TableBorders.js
@@ -1,9 +1,9 @@
 import Element from './Element'
 
-function createBorder(style, size, space, color, shadow) {
+function createBorder(size, style, space, color, shadow) {
   return {
-    '@w:val': style ? `${style}` : 'single',
     '@w:sz': size ? `${size}` : '0',
+    '@w:val': style ? `${style}` : 'single',
     '@w:space': space ? `${space}` : '0',
     '@w:color': color ? `${color}` : '000000',
     '@w:shadow': shadow ? 'true' : 'false'
@@ -17,41 +17,58 @@ export default class extends Element {
   }
 
   finalize (contents) {
-    if (this.top) contents.unshift(this.top)
-    if (this.bottom) contents.unshift(this.bottom)
-    if (this.start) contents.unshift(this.start)
-    if (this.end) contents.unshift(this.end)
-    if (this.hInside) contents.unshift(this.hInside)
     if (this.vInside) contents.unshift(this.vInside)
+    if (this.hInside) contents.unshift(this.hInside)
+    if (this.end) contents.unshift(this.end)
+    if (this.start) contents.unshift(this.start)
+    if (this.bottom) contents.unshift(this.bottom)
+    if (this.top) contents.unshift(this.top)
   }
 
-  setTop(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setTop(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.top = new Element({ 'w:top': border }, '["w:top"]')
   }
 
-  setBottom(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setBottom(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.bottom = new Element({ 'w:bottom': border }, '["w:bottom"]')
   }
 
-  setStart(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setStart(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.start = new Element({ 'w:start': border }, '["w:start"]')
   }
 
-  setEnd(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setEnd(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.end = new Element({ 'w:end': border }, '["w:end"]')
   }
 
-  setHInside(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setHInside(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.hInside = new Element({ 'w:insideH': border }, '["w:insideH"]')
   }
 
-  setVInside(style, size, space, color, shadow) {
-    const border = createBorder(style, size, space, color, shadow)
+  setVInside(size, style, space, color, shadow) {
+    const border = createBorder(size, style, space, color, shadow)
     this.vInside = new Element({ 'w:insideV': border }, '["w:insideV"]')
+  }
+
+  setOutside(size, style, space, color, shadow) {
+    this.setTop(size, style, space, color, shadow)
+    this.setStart(size, style, space, color, shadow)
+    this.setEnd(size, style, space, color, shadow)
+    this.setBottom(size, style, space, color, shadow)
+  }
+
+  setInside(size, style, space, color, shadow) {
+    this.setHInside(size, style, space, color, shadow)
+    this.setVInside(size, style, space, color, shadow)
+  }
+
+  setAll(size, style, space, color, shadow) {
+    this.setInside(size, style, space, color, shadow)
+    this.setOutside(size, style, space, color, shadow)
   }
 }
--- test/TableFormat.js
+++ test/TableFormat.js
@@ -17,10 +17,15 @@ describe('#TableFormat', () => {
     })
   })
   describe('#toXml', () => {
-    it('should render borders correctly', () => {
+    it('should render outside borders correctly', () => {
       let fmt = new jsdocx.TableFormat()
-      fmt.addBorders().setStart('single', 12)
-      assert.equal(fmt.toXml(), '<w:tblPr><w:tblBorders><w:start w:val="single" w:sz="12" w:space="0" w:color="000000" w:shadow="false"/></w:tblBorders></w:tblPr>')
+      fmt.addBorders().setOutside(12)
+      assert.equal(fmt.toXml(), '<w:tblPr><w:tblBorders><w:top w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/><w:bottom w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/><w:start w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/><w:end w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/></w:tblBorders></w:tblPr>')
+    })
+    it('should render inside borders correctly', () => {
+      let fmt = new jsdocx.TableFormat()
+      fmt.addBorders().setInside(12)
+      assert.equal(fmt.toXml(), '<w:tblPr><w:tblBorders><w:insideH w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/><w:insideV w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/></w:tblBorders></w:tblPr>')
     })
   })
 })
