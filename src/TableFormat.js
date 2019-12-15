c9030ad95e9c1da8e1abf93a556d6f6c15dd16f3
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2019-12-15 21:39:50 +0100

    Add support for setting table width

--- src/TableFormat.js
+++ src/TableFormat.js
@@ -3,14 +3,18 @@ import TableBorders from './TableBorders'
 
 export default class extends Element {
   constructor(
-    borders
+    borders,
+    width,
+    widthUnitType
   ) {
     super({ 'w:tblPr': {} }, '["w:tblPr"]')
     this.setBorders(borders || null)
+    this.setWidth(width, widthUnitType)
   }
 
   finalize (contents) {
     if (this.borders) contents.unshift(this.borders)
+    if (this.width) contents.unshift(this.width)
   }
 
   addBorders () {
@@ -28,5 +32,16 @@ export default class extends Element {
 
   getBorders () {
     return this.borders
+  }
+
+  setWidth (width, unitType) {
+    if (width) {
+      this.width = new Element({ 'w:tblW': {
+        '@w:type': unitType || 'dxa',
+        '@w:w': width
+      } }, '["w:tblW"]')
+    } else {
+      this.width = null
+    }
   }
 }
--- test/TableFormat.js
+++ test/TableFormat.js
@@ -27,5 +27,10 @@ describe('#TableFormat', () => {
       fmt.addBorders().setInside(12)
       assert.equal(fmt.toXml(), '<w:tblPr><w:tblBorders><w:insideH w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/><w:insideV w:sz="12" w:val="single" w:space="0" w:color="000000" w:shadow="false"/></w:tblBorders></w:tblPr>')
     })
+    it('should render cell width correctly', () => {
+      let fmt = new jsdocx.Table().addFormat()
+      fmt.setWidth('33.3%', 'pct')
+      assert.equal(fmt.toXml(), '<w:tblPr><w:tblW w:type="pct" w:w="33.3%"/></w:tblPr>')
+    })
   })
 })
